8354b78e709700c69bc97247d8ccfccd1554459d
RomanChen <romam@romamdeMacBook-Pro.local>
2020-02-25 22:33:36 +0800

    resetAll()

--- all.js
+++ all.js
@@ -4,6 +4,7 @@ var vm = new Vue({
     title: 'ㄅㄆㄇ',
     alphabets: ['ㄅ', 'ㄆ', 'ㄇ', 'ㄈ', 'ㄉ', 'ㄊ', 'ㄋ', 'ㄌ', 'ㄍ', 'ㄎ', 'ㄏ', 'ㄐ', 'ㄑ', 'ㄒ', 'ㄓ', 'ㄔ', 'ㄕ', 'ㄖ', 'ㄗ', 'ㄘ', 'ㄙ', 'ㄧ', 'ㄨ', 'ㄩ', 'ㄚ', 'ㄛ', 'ㄜ', 'ㄝ', 'ㄞ', 'ㄟ', 'ㄠ', 'ㄡ', 'ㄢ', 'ㄣ', 'ㄤ', 'ㄥ', 'ㄦ'],
     alphabetList: [],
+    countDown: 10
 
   },
 
@@ -21,10 +22,28 @@ var vm = new Vue({
       item.deleted = true
     },
 
-    // clearAll: function () {
-    //   alphabetList.deleted = false
-    // },
+    clearAll: function () {
+      for (let i = 0; i < this.alphabets.length; i++) {
+        this.alphabetList[i].deleted = false
+      }
+      this.countDown = 10
+    },
 
+    resetNum: function(){
+      clearTimeout(countDownTimer)
+      this.countDown = 10
+    },
 
+    countDownTimer:function() {
+      if (this.countDown > 0) {
+        setTimeout(() => {
+          this.countDown -= 1  
+          this.countDownTimer()
+        }, 1000)
+      }
+    }
   }
+
+
 })
+
