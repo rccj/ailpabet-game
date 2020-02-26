c906d9dfe7da3c2d3c88873066ac1125498a7639
RomanChen <romam@romamdeMacBook-Pro.local>
2020-02-26 14:59:00 +0800

    clear()is good

--- all.js
+++ all.js
@@ -4,7 +4,7 @@ var vm = new Vue({
     title: 'ㄅㄆㄇ',
     alphabets: ['ㄅ', 'ㄆ', 'ㄇ', 'ㄈ', 'ㄉ', 'ㄊ', 'ㄋ', 'ㄌ', 'ㄍ', 'ㄎ', 'ㄏ', 'ㄐ', 'ㄑ', 'ㄒ', 'ㄓ', 'ㄔ', 'ㄕ', 'ㄖ', 'ㄗ', 'ㄘ', 'ㄙ', 'ㄧ', 'ㄨ', 'ㄩ', 'ㄚ', 'ㄛ', 'ㄜ', 'ㄝ', 'ㄞ', 'ㄟ', 'ㄠ', 'ㄡ', 'ㄢ', 'ㄣ', 'ㄤ', 'ㄥ', 'ㄦ'],
     alphabetList: [],
-    countDown: 10
+    countDownFrom: 10
 
   },
 
@@ -15,7 +15,7 @@ var vm = new Vue({
   methods: {
     getAlphabetList: function () {
       for (let i = 0; i < this.alphabets.length; i++) {
-        this.alphabetList.push({ alphabet: this.alphabets[i], deleted: false })
+        this.alphabetList.push({ alphabet: this.alphabets[i], deleted: false, counting: false })
       }
     },
     deleteAlph: function (item) {
@@ -26,22 +26,33 @@ var vm = new Vue({
       for (let i = 0; i < this.alphabets.length; i++) {
         this.alphabetList[i].deleted = false
       }
-      this.countDown = 10
+      clearTimeout(this.time)
+      this.countDownFrom = 10
     },
 
-    resetNum: function(){
-      clearTimeout(countDownTimer)
-      this.countDown = 10
+
+    countDownTimer: function (item) {
+      if (item.counting === false) {
+        clearTimeout(this.time)
+        item.counting = true
+        this.countDownFrom = 10
+        this.countingDown()
+      }
+
     },
 
-    countDownTimer:function() {
-      if (this.countDown > 0) {
-        setTimeout(() => {
-          this.countDown -= 1  
-          this.countDownTimer()
+
+    countingDown: function () {
+      if (this.countDownFrom > 0) {
+        let mytime = this
+        this.time = setTimeout(() => {
+          this.countDownFrom -= 1
+          this.countingDown()
         }, 1000)
       }
-    }
+    },
+
+
   }
 
 
